0ee0e1ad3b069fe60548ec7165fc78cb5177e3bc
MOHAMAD ALMEARI <mohamet.almeari@gmail.com>
2023-06-04 02:49:48 +0000

    Style todo list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/app.css';
 import TodosLogic from './components/TodosLogic';
+import Header from "./components/Header";
 
-function App() {
+
+
+const App = () => {
   return (
-    <TodosLogic />
+    <div className="wrapper">
+      <div className="todos">
+      
+        <Header />
+        <TodosLogic />
+      </div>
+    </div>
   );
-}
-
+};
 export default App;
